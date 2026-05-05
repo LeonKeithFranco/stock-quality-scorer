05d2f2c0f18283c5494a0151588aa0768b6b422c
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-05 15:39:26 -0700

    feat(ml,data): add script to pull fundametals for s&p 500 constituents

---
+++ ml/scripts/get_fundamentals.py
@@ -0,0 +1,85 @@
+import csv
+import time
+from pathlib import Path
+
+import pandas as pd
+import yfinance as yf
+from yfinance.exceptions import YFRateLimitError
+
+from scripts._utils import FILE_BASE_NAME, get_today_date_as_str
+
+_DATA_FOLDER_PATH = Path(__file__).parent.parent / "data"
+_CSV_FOLDER_PATH = _DATA_FOLDER_PATH / "csvs"
+_PARQUEST_FOLDER_PATH = _DATA_FOLDER_PATH / "parquets"
+_MAX_ATTEMPTS = 6
+_TARGET_KEYS = [
+    "trailingPE",
+    "priceToBook",
+    "returnOnEquity",
+    "debtToEquity",
+    "revenueGrowth",
+    "grossMargins",
+    "operatingMargins",
+    "profitMargins",
+]
+
+
+def main():
+    today_date_str = get_today_date_as_str()
+    full_file_name = f"{FILE_BASE_NAME}{today_date_str}.parquet"
+    file_path = _PARQUEST_FOLDER_PATH / full_file_name
+
+    if file_path.exists():
+        return
+
+    snp_500_constituents_files = list(_CSV_FOLDER_PATH.glob("*.csv"))
+
+    if not snp_500_constituents_files:
+        raise FileNotFoundError(
+            "There are no snp_500_constituents_*.csv files. Please run get_snp_500.py script first."
+        )
+
+    most_recent_file = (
+        snp_500_constituents_files[0]
+        if len(snp_500_constituents_files) == 1
+        else sorted(snp_500_constituents_files)[-1]
+    )
+
+    tickers: list[str] = []
+
+    with open(most_recent_file, "r") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            tickers.append(row["Symbol"])
+
+    snp_500_constituents_info = []
+
+    for ticker in tickers:
+        wait_time = 1
+        info_retrieved = False
+        attempts = 0
+
+        while not info_retrieved and attempts < _MAX_ATTEMPTS:
+            attempts += 1
+            try:
+                yf_stock_info = yf.Ticker(ticker).info
+                info_retrieved = True
+            except YFRateLimitError:
+                time.sleep(wait_time)
+                wait_time *= 1.5
+
+        if not info_retrieved:
+            continue
+
+        stock_info = {"symbol": ticker} | {
+            k: yf_stock_info.get(k, None) for k in _TARGET_KEYS
+        }
+        snp_500_constituents_info.append(stock_info)
+
+    df = pd.DataFrame(snp_500_constituents_info)
+
+    df.to_parquet(file_path)
+
+
+if __name__ == "__main__":
+    main()
