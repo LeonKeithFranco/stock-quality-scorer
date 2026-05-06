78c039b920e3cd1d19ac0948b98817c4d6abce22
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-06 10:54:47 -0700

    feat(ml,data): add script to grab all data in one go

---
+++ ml/scripts/grab_data.py
@@ -0,0 +1,25 @@
+import asyncio
+
+from scripts.get_fundamentals import main as get_fundamentals
+from scripts.get_price_history import main as get_price_history
+from scripts.get_snp_500 import main as get_snp_500
+
+
+async def _price_and_fundamentals_async_runner():
+    async with asyncio.TaskGroup() as tg:
+        tg.create_task(asyncio.to_thread(get_fundamentals))
+        tg.create_task(asyncio.to_thread(get_price_history))
+
+
+def main():
+    print("Retrieving S&P 500 constituents...")
+    get_snp_500()
+    print("...S&P retrieved.")
+
+    print("Retrieving price histories and fundamentals for S&P 500 constituents...")
+    asyncio.run(_price_and_fundamentals_async_runner())
+    print("...Price histories and fundamentals retrieved.")
+
+
+if __name__ == "__main__":
+    main()
