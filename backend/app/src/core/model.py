a922cf51e5c27bfeaac311dba4fbdd75024bbfde
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-11 20:34:18 -0700

    feat(backend): add function to load model

---
+++ backend/app/src/core/model.py
@@ -0,0 +1,14 @@
+from functools import lru_cache
+from pathlib import Path
+
+import joblib
+from sklearn.calibration import CalibratedClassifierCV
+
+_MODEL_FILE_PATH = (
+    Path(__file__).parent.parent.parent.parent / "data" / "rf_calibrated.joblib"
+)
+
+
+@lru_cache
+def get_model() -> CalibratedClassifierCV:
+    return joblib.load(_MODEL_FILE_PATH)
