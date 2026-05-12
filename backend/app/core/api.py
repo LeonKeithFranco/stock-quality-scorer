1e6f07c89a57390dc2ef684c78af1cdf86432264
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-12 11:38:06 -0700

    refactor(backend): move column names to constants file

--- backend/app/core/api.py
+++ backend/app/core/api.py
@@ -4,19 +4,10 @@ import yfinance as yf
 from cachetools import TTLCache
 from cachetools_async import cached
 
+from app.core.constants import TARGET_INFO_KEYS
+
 _MAX_NUM_OF_STOCK_INFO = 525
 _TTL_IN_SECONDS = 86_400
-_TARGET_INFO_KEYS = [
-    "ticker",
-    "trailingPE",
-    "priceToBook",
-    "returnOnEquity",
-    "debtToEquity",
-    "revenueGrowth",
-    "grossMargins",
-    "operatingMargins",
-    "profitMargins",
-]
 
 
 def _get_fundamentals_helper(ticker: str) -> dict:
@@ -26,6 +17,6 @@ def _get_fundamentals_helper(ticker: str) -> dict:
 @cached(cache=TTLCache(maxsize=_MAX_NUM_OF_STOCK_INFO, ttl=_TTL_IN_SECONDS))
 async def get_fundamentals(ticker: str) -> dict:
     funadmental_info = await asyncio.to_thread(_get_fundamentals_helper, ticker)
-    target_info = {key: funadmental_info[key] for key in _TARGET_INFO_KEYS}
+    target_info = {key: funadmental_info[key] for key in TARGET_INFO_KEYS}
 
     return target_info
---
+++ backend/app/core/constants.py
@@ -0,0 +1,11 @@
+TARGET_INFO_KEYS = [
+    "ticker",
+    "trailingPE",
+    "priceToBook",
+    "returnOnEquity",
+    "debtToEquity",
+    "revenueGrowth",
+    "grossMargins",
+    "operatingMargins",
+    "profitMargins",
+]
