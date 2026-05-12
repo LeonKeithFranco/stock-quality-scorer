5d5a97bc20fb969f63e8736ea4131a1f116e9098
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-12 15:02:26 -0700

    refactor(backend): add logic to gaurd against missing keys in fundamental dict

--- backend/app/core/api.py
+++ backend/app/core/api.py
@@ -17,6 +17,6 @@ def _get_fundamentals_helper(ticker: str) -> dict:
 @cached(cache=TTLCache(maxsize=_MAX_NUM_OF_STOCK_INFO, ttl=_TTL_IN_SECONDS))
 async def get_fundamentals(ticker: str) -> dict:
     fundamental_info = await asyncio.to_thread(_get_fundamentals_helper, ticker)
-    target_info = {key: fundamental_info[key] for key in TARGET_INFO_KEYS}
+    target_info = {key: fundamental_info.get(key, None) for key in TARGET_INFO_KEYS}
 
     return target_info
