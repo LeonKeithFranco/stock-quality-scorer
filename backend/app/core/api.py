cef8196e27e9a91495027f226f351827460249af
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-12 14:55:09 -0700

    refactor(backend): fix typo in variable name

--- backend/app/core/api.py
+++ backend/app/core/api.py
@@ -16,7 +16,7 @@ def _get_fundamentals_helper(ticker: str) -> dict:
 
 @cached(cache=TTLCache(maxsize=_MAX_NUM_OF_STOCK_INFO, ttl=_TTL_IN_SECONDS))
 async def get_fundamentals(ticker: str) -> dict:
-    funadmental_info = await asyncio.to_thread(_get_fundamentals_helper, ticker)
-    target_info = {key: funadmental_info[key] for key in TARGET_INFO_KEYS}
+    fundamental_info = await asyncio.to_thread(_get_fundamentals_helper, ticker)
+    target_info = {key: fundamental_info[key] for key in TARGET_INFO_KEYS}
 
     return target_info
