717f1024cbc8802f83a6c235b19a9ba230609ef4
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-12 14:41:05 -0700

    fix(backend): create hashing function for dataframe

--- backend/app/core/model.py
+++ backend/app/core/model.py
@@ -1,11 +1,19 @@
+import hashlib
 from pathlib import Path
 
 import joblib
 import pandas as pd
 from cachetools import LRUCache, cached
+from pandas.core.util.hashing import hash_pandas_object
 from sklearn.calibration import CalibratedClassifierCV
 
 _MODEL_FILE_PATH = Path(__file__).parent.parent.parent / "data" / "rf_calibrated.joblib"
+
+
+def _custom_df_hash(df: pd.DataFrame) -> str:
+    hashes = hash_pandas_object(df)
+
+    return hashlib.sha256(hashes.values.tobytes()).hexdigest()
 
 
 @cached(cache=LRUCache(maxsize=1))
@@ -13,7 +21,7 @@ def get_model() -> CalibratedClassifierCV:
     return joblib.load(_MODEL_FILE_PATH)
 
 
-@cached(cache=LRUCache(maxsize=525))
+@cached(cache=LRUCache(maxsize=525), key=_custom_df_hash)
 def predict(fundamentals: pd.DataFrame) -> float:
     prediction = get_model().predict_proba(fundamentals)
     outperform_probability = prediction[:, 1]
