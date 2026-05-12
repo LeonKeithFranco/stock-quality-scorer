ca048da86f33431426e11dbcf16c7d4e98d50332
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-12 11:26:10 -0700

    refactor(backend): change caching method

--- backend/app/core/model.py
+++ backend/app/core/model.py
@@ -1,4 +1,3 @@
-from functools import lru_cache
 from pathlib import Path
 
 import joblib
@@ -9,7 +8,7 @@ from sklearn.calibration import CalibratedClassifierCV
 _MODEL_FILE_PATH = Path(__file__).parent.parent.parent / "data" / "rf_calibrated.joblib"
 
 
-@lru_cache
+@cached(cache=LRUCache(maxsize=1))
 def get_model() -> CalibratedClassifierCV:
     return joblib.load(_MODEL_FILE_PATH)
 
