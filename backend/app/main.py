9c7bd4be918493239ff1369ac7ea9af3f94da161
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-11 20:50:05 -0700

    chore(backend): remove uneeded root endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,11 +5,6 @@ from app.src.core.lifespan import lifespan
 app = FastAPI(lifespan=lifespan)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello, world!"}
-
-
 @app.get("/health")
 async def health_check():
     return {"status": "ok"}
