9843e50b12d52abd0bca8e6a428cb5fc0ba05a36
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-13 09:52:18 -0700

    feat(backend): call predict snp 500 at startup time

--- backend/app/core/lifespan.py
+++ backend/app/core/lifespan.py
@@ -3,11 +3,15 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app.core.model import get_model
+from app.domain.service import Service
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # called to make sure model gets loaded into memory
     get_model()
+
+    # called to preload snp stock predictions into cache
+    await Service().predict_outperfromance_of_snp_500()
 
     yield
