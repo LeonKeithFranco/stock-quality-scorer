21df85c8d93025b2af376b2107f586c19d21ce66
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-13 08:43:31 -0700

    refactor(backend): add retry logic when getting fundamentals in the backend

--- backend/app/core/api.py
+++ backend/app/core/api.py
@@ -1,20 +1,35 @@
 import asyncio
+import random
+import time
 
 import yfinance as yf
 from cachetools import TTLCache
 from cachetools_async import cached
+from yfinance.exceptions import YFRateLimitError
 
 from app.core.constants import TARGET_INFO_KEYS
 from app.core.exceptions import StockMissingError
 
 _MAX_NUM_OF_STOCK_INFO = 525
 _TTL_IN_SECONDS = 86_400
+_MAX_ATTEMPTS = 7
 
 
 def _get_fundamentals_helper(ticker: str) -> dict:
-    stock_info = yf.Ticker(ticker).info
+    stock_info = None
+    wait_time = 1
+    attempts = 0
 
-    if "symbol" not in stock_info:
+    while not stock_info and attempts < _MAX_ATTEMPTS:
+        attempts += 1
+        try:
+            stock_info = yf.Ticker(ticker).info
+        except YFRateLimitError:
+            time.sleep(wait_time)
+            wait_time *= 2
+            wait_time += random.uniform(0.0, wait_time * 0.1)
+
+    if not stock_info or "symbol" not in stock_info:
         raise StockMissingError(ticker=ticker)
 
     return stock_info
