b052161b043eaa691bfc6960ef688073cc62efe9
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-13 09:58:32 -0700

    feat(backend,config): create config file and add app name

---
+++ backend/app/core/config.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+from cachetools import LRUCache, cached
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class _Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=Path(__file__).parent.parent.parent / ".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="ignore",
+    )
+
+    app_name: str = Field(min_length=1, max_length=20)
+
+
+@cached(cache=LRUCache(maxsize=1))
+def get_settings() -> _Settings:
+    return _Settings()  # ty: ignore[missing-argument]
--- backend/app/core/lifespan.py
+++ backend/app/core/lifespan.py
@@ -2,12 +2,15 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+from app.core.config import get_settings
 from app.core.model import get_model
 from app.domain.service import Service
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    app.title = get_settings().app_name
+
     # called to make sure model gets loaded into memory
     get_model()
 
