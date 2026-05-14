431d287b3bb3b17a672a9f5b43b18b35000f183a
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-14 13:51:59 -0700

    refactor(frontend): move table creation logic to separate function

--- frontend/main.py
+++ frontend/main.py
@@ -1,11 +1,33 @@
 from dataclasses import asdict
 
+import httpx
 import pandas as pd
 import streamlit as st
 from src.core import api
+from src.core.api import PredictionResponse
 from src.core.config import get_settings
 
 st.header(get_settings().app_name)
+
+
+def _create_table(predictions: list[PredictionResponse]) -> None:
+    df_prediction = pd.DataFrame([asdict(prediction) for prediction in predictions])
+    df_prediction["outperformance_probability"] = (
+        df_prediction["outperformance_probability"] * 100
+    ).round(2)
+    df_prediction["predicted_class"] = df_prediction["predicted_class"].map(
+        lambda v: "Y" if v == 1 else "N"
+    )
+    df_prediction.columns = [
+        "Ticker",
+        "Probability of Outperformance",
+        "Predicted to Beat S&P 500",
+    ]
+
+    column_config = {
+        "Probability of Outperformance": st.column_config.NumberColumn(format="%.2f%%")
+    }
+    st.dataframe(df_prediction, column_config=column_config, hide_index=True)
 
 
 @st.fragment
@@ -20,28 +42,7 @@ def single_predict():
                 with st.spinner("Loading..."):
                     prediction = api.predict(ticker)
 
-                df_prediction = pd.DataFrame([asdict(prediction)])
-                df_prediction["outperformance_probability"] = (
-                    df_prediction["outperformance_probability"] * 100
-                ).round(2)
-                df_prediction["predicted_class"] = df_prediction["predicted_class"].map(
-                    lambda v: "Y" if v == 1 else "N"
-                )
-                df_prediction.columns = [
-                    "Ticker",
-                    "Probability of Outperformance",
-                    "Predicted to Beat S&P 500",
-                ]
-
-                column_config = {
-                    "Probability of Outperformance": st.column_config.NumberColumn(
-                        format="%.2f%%"
-                    )
-                }
-                st.dataframe(
-                    df_prediction, column_config=column_config, hide_index=True
-                )
-
+                _create_table([prediction])
             except Exception as e:
                 st.error(f"Unable to get data: {e}")
         else:
@@ -56,24 +57,6 @@ try:
     with st.spinner("Loading..."):
         snp_500_predictions = api.predict_snp_500()
 
-    df_snp_500_predictions = pd.DataFrame(
-        [asdict(prediction) for prediction in snp_500_predictions]
-    )
-    df_snp_500_predictions["outperformance_probability"] = (
-        df_snp_500_predictions["outperformance_probability"] * 100
-    ).round(2)
-    df_snp_500_predictions["predicted_class"] = df_snp_500_predictions[
-        "predicted_class"
-    ].map(lambda v: "Y" if v == 1 else "N")
-    df_snp_500_predictions.columns = [
-        "Ticker",
-        "Probability of Outperformance",
-        "Predicted to Beat S&P 500",
-    ]
-
-    column_config = {
-        "Probability of Outperformance": st.column_config.NumberColumn(format="%.2f%%")
-    }
-    st.dataframe(df_snp_500_predictions, column_config=column_config, hide_index=True)
+    _create_table(snp_500_predictions)
 except Exception as e:
     st.error(f"Unable to get data: {e}")
