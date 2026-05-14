6aba09e819461e235ee31bc77f1a5d78218c0f65
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-14 13:41:35 -0700

    feat(frontend): add section to predict single stocks

--- frontend/main.py
+++ frontend/main.py
@@ -8,6 +8,48 @@ from src.core.config import get_settings
 st.header(get_settings().app_name)
 
 
+@st.fragment
+def single_predict():
+    with st.form("ticker_predict_form"):
+        ticker = st.text_input("Enter Ticker", placeholder="AAPL, MSFT, KO, etc.")
+        submitted = st.form_submit_button("Predict")
+
+    if submitted:
+        if ticker:
+            try:
+                with st.spinner("Loading..."):
+                    prediction = api.predict(ticker)
+
+                df_prediction = pd.DataFrame([asdict(prediction)])
+                df_prediction["outperformance_probability"] = (
+                    df_prediction["outperformance_probability"] * 100
+                ).round(2)
+                df_prediction["predicted_class"] = df_prediction["predicted_class"].map(
+                    lambda v: "Y" if v == 1 else "N"
+                )
+                df_prediction.columns = [
+                    "Ticker",
+                    "Probability of Outperformance",
+                    "Predicted to Beat S&P 500",
+                ]
+
+                column_config = {
+                    "Probability of Outperformance": st.column_config.NumberColumn(
+                        format="%.2f%%"
+                    )
+                }
+                st.dataframe(
+                    df_prediction, column_config=column_config, hide_index=True
+                )
+
+            except Exception as e:
+                st.error(f"Unable to get data: {e}")
+        else:
+            st.warning("Please enter a stock ticker.")
+
+
+single_predict()
+
 st.divider()
 
 try:
