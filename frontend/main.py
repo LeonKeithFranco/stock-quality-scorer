f74306dba3026dab6b145c2f7156dd6e89d27aa1
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-14 11:23:02 -0700

    refactor(frontend): format values of table

--- frontend/main.py
+++ frontend/main.py
@@ -17,11 +17,16 @@ except Exception as e:
 df_snp_500_predictions = pd.DataFrame(
     [asdict(prediction) for prediction in snp_500_predictions]
 )
+df_snp_500_predictions["outperformance_probability"] = df_snp_500_predictions[
+    "outperformance_probability"
+].map(lambda v: f"{v * 100:.2f}%")
+df_snp_500_predictions["predicted_class"] = df_snp_500_predictions[
+    "predicted_class"
+].map(lambda v: "Y" if v == 1 else "N")
 df_snp_500_predictions.columns = [
     "Ticker",
     "Probability of Outperformance",
     "Beat S&P 500",
 ]
-
 
 st.dataframe(df_snp_500_predictions)
